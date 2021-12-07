953a7e8d21fcc3845649ebb367061c1f9319899a
TravisWongX <travis.x.wong@gmail.com>
2021-12-07 17:44:05 +0800

    .then和.catch状态变化

--- 6-JS-async-advanced/promise.js
+++ 6-JS-async-advanced/promise.js
@@ -1,5 +1,27 @@
-const p1 = Promise.resolve(1)
-p1.then((result) => console.log(result)).catch((err) => console.error(err))
+// const p1 = Promise.resolve(1)
+// p1.then((result) => console.log(result)).catch((err) => console.error(err))
 
-const p2 = Promise.reject(new Error('err'))
-p2.then(null, () => console.log('then err')).catch((err) => console.error(err))
+// const p2 = Promise.reject(new Error('err'))
+// p2.then(null, () => console.log('then err')).catch((err) => console.error(err))
+
+const p1 = Promise.resolve().then(() => {
+  return 100
+})
+console.log('p1', p1) // fulfilled
+
+const p2 = Promise.resolve().then(() => {
+  throw new Error('err')
+})
+console.log('p2', p2) // rejected
+
+const p3 = Promise.reject('err').catch((err) => {
+  return err
+})
+
+console.log('p3', p3) // fulfilled
+
+const p4 = Promise.reject('err').catch(() => {
+  throw new Error('catch err')
+})
+
+console.log('p4', p4) // rejected
